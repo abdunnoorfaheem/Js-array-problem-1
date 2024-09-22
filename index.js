780ffb961f496e444459c7c90446c462dd0ee839
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-09-22 15:23:54 +0600

    add

---
+++ index.js
@@ -0,0 +1,54 @@
+//problem -1
+
+
+let fruits = ["apple", "banana", "cherry"];
+
+// console.log(fruits.toString());
+
+//problem--2
+
+
+let arrElement = [10, 20, 30, 40, 50];
+
+// console.log("Third element is ",
+//     arrElement[2]);
+
+//problem--3
+
+
+let color = ["red", "green", "blue"];
+
+// console.log(color.join("-"));
+
+//problem--4
+
+
+let numArray = [1, 2, 3, 4, 5];
+
+// console.log(numArray);
+
+numArray.pop();
+// console.log("remove last element",
+//     numArray);
+
+
+//problem--5
+
+let fruitsArray = ["apple", "banana"];
+
+// console.log(fruitsArray);
+fruitsArray.push("grape");
+
+// console.log("add new element in the last :",
+//     fruitsArray);
+
+//problem--6
+
+
+let vehicle = ["car", "bike", "bus"];
+
+console.log(vehicle);
+
+vehicle.shift();
+
+console.log("remove first element :", vehicle);
